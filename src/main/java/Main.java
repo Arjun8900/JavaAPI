b2343fdb94a8003354d8a54ec8196b830bb89ca8
Kanwal <akanwal@informatica.com>
2022-12-11 10:35:26 +0530

    Updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
     public static void main(String[] args)  {
-//        ClientCall clientCall = new ClientCall();
-//        log.info("Output {}", clientCall.getDataUsingOriginal());
+        /*
+            We need TypeReference bcoz our method is generic. The type parameter T cannot be reified at runtime,
+            even if we pass a TypeReference instance with the type parameter T.
+         */
+        ObjectMapper mapper = new ObjectMapper();
+        TypeReference<ClientCallData> typeReference = new TypeReference<ClientCallData>(){};
 
         IClient<ClientCallData> clientCall2 = new ClientCall<>();
-        ClientCallData accountList = new ObjectMapper().convertValue(clientCall2.getData("reqres.in", 0, "/api/products/3"), new TypeReference<ClientCallData>(){});
+        ClientCallData accountList = mapper.convertValue(clientCall2.getData("reqres.in", 0, "/api/products/3"), typeReference);
         System.out.println(accountList);
     }
 }
